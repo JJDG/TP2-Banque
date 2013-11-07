76bed70b705561d8c72d600755a7ba06e488b16b
Josselin <Josselin@JosselinWeshPc>
2013-11-07 21:47:37 +0100

    Constructeurs

--- src/Epargne.java
+++ src/Epargne.java
@@ -1,4 +1,14 @@
 
 public class Epargne extends Compte{
-
+	private double taux;
+	
+	public Epargne(){
+		super();
+		taux = 0;
+	}
+	
+	public Epargne(Client leClient, int numero, int solde, double unTaux){
+		super(leClient, numero, solde);
+		taux = unTaux;
+	}
 }
